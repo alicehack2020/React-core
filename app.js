d30787f9e6f8e3a7b4172482d7da36c170081649
alicehack2020 <alicehack2020@gmail.com>
2023-12-20 22:33:13 +0530

    react using cdn

---
+++ app.js
@@ -0,0 +1,29 @@
+// const heading = React.createElement("h1", { id: "heading" }, "hello world");
+
+// console.log(heading); //object
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading); //convet into html and put into dom
+
+//1. build this here--------------------->
+// <div id="parent">
+//   <div id="child">
+//     <h1>hello world 1</h1>
+//     <h2>hello world 2</h2>
+//   </div>
+// </div>;
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hello world 1"),
+    React.createElement("h1", {}, "hello world 2"),
+  ])
+);
+
+//jsx will help us to make this code to readable code 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# day 1 inception
+
+    https://unpkg.com/react@18.2.0/umd/react.development.js
+    https://unpkg.com/react-dom@18/umd/react-dom.development.js
+
+    # link https://legacy.reactjs.org/docs/cdn-links.html
