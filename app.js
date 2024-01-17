c07351228a9673ca9ec3f2dcc66d18d1571837c6
alicehack2020 <alicehack2020@gmail.com>
2024-01-17 23:32:48 +0530

    component composition

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 
 const jsxHeading = <h1 className="heading">Welcome to Jsx</h1>;
 
+const Title = () => (
+  <div>
+    <h1 className="demo">demo</h1>
+  </div>
+);
+
+
+//component composition
+const Component = () => (
+  <div>
+    <h1>main</h1>
+    <Title />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<Component />);
