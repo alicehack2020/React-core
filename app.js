8d66a490e01960a7c35206d431a73b07b5ca7682
alicehack2020 <alicehack2020@gmail.com>
2024-01-17 21:52:01 +0530

    babel and jsx start

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = React.createElement("h1", {}, "here h1");
-const h2 = React.createElement("h2", {}, "here h2");
-
-const h3 = React.createElement("h3", {}, "here h3");
-const h4 = React.createElement("h4", {}, "here h4");
-
-const child1 = React.createElement("div", {}, [h1, h2]);
-const child2 = React.createElement("div", {}, [h3, h4]);
-
-const parent = React.createElement("div", {}, [child1, child2]);
+const jsxHeading = <h1 className="heading">Welcome to Jsx</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
